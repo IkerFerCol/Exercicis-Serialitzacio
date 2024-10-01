c4acb02df6648ebea157945097d348d748e6be01
ikefercol <ikefercol@alu.edu.gva.es>
2024-10-01 12:59:14 +0200

    1/10/24

---
+++ E2/src/Main.java
@@ -0,0 +1,74 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Assignatura[] assignaturas = new Assignatura[6];
+        Scanner scanner = new Scanner(System.in);
+        String[] nombresassignaturas = {"Programacion", "Lenguaje", "Entornos", " Base Datos", "Sistemas", "Fol"};
+
+        for (int i = 0; i < assignaturas.length; i++) {
+            assignaturas[i] = new Assignatura(nombresassignaturas[i]);
+            System.out.print("Introduce la nota de la asignatura " + nombresassignaturas[i] + ": ");
+            double nota = scanner.nextDouble();
+            assignaturas[i].setNota(nota);
+        }
+
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("Assignaturas.txt"))) {
+            oos.writeObject(assignaturas);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("Assignaturas.txt"))){
+            Assignatura[] deserializado = (Assignatura[]) ois.readObject();
+            double suma = 0;
+            for (Assignatura assignatura : deserializado) {
+                System.out.println(assignatura.toString());
+                suma += assignatura.getNota();
+            }
+            double media = suma / deserializado.length;
+            System.out.println("La media es: " + media);
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        scanner.close();
+
+    }
+}
+
+
+class Assignatura implements Serializable {
+    private String nombre;
+    private double nota;
+
+    public Assignatura(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+    public String toString() {
+        return "Nombre: " + nombre + ", Nota: " + nota;
+    }
+}
